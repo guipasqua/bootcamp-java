448e03f64792bcf06edb35378384f53c99b43342
guipasqua <guipasqualetti@gmail.com>
2026-01-06 19:40:30 -0300

    feat-tipos-variaveis-final

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Tipos primitivos em Java
+        int numeroInteiro = 10;               // Tipo inteiro
+        double numeroDecimal = 20.5;          // Tipo ponto flutuante
+        char caractere = 'A';                 // Tipo caractere
+        boolean valorBooleano = true;         // Tipo booleano
+
+        // Exibindo os valores das variáveis
+        System.out.println("Número Inteiro: " + numeroInteiro);
+        System.out.println("Número Decimal: " + numeroDecimal);
+        System.out.println("Caractere: " + caractere);
+        System.out.println("Valor Booleano: " + valorBooleano);
     }
 }
